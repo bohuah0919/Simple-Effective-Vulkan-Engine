4430153a657de25d0baff7f10a246e53cfcb04c0
bohuah0919 <98621364+bohuah0919@users.noreply.github.com>
2023-11-15 17:31:02 -0800

    Delete Texture.h

--- Texture.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "vk_types.h"
-#include "vk_engine.h"
-
-namespace vkutil {
-
-	bool load_image_from_file(VulkanEngine& engine, std::string file, AllocatedImage& outImage);
-
-}
